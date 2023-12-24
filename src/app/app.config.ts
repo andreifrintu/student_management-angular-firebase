00d2f5c14c9ee3505a260cbd180d7960c34afa95
Andrew <frintu.andrei07@gmail.com>
2023-12-24 20:12:15 +0200

    added firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat/';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { firebaseConfig } from './firebase-config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(AngularFireModule.initializeApp(firebaseConfig)),
+  ]
 };
